b0c96796d32d2fff7d0ecc5da3cb806189ff5fdc
garffield <gabrielfranklinbarcellos@gmail.com>
2024-09-09 17:13:24 -0300

    Projeto Finalizado

---
+++ dados.js
@@ -0,0 +1,27 @@
+let dados = [
+    
+    {
+        titulo : 'Charizard',
+        descricao : 'Charizard, O Dragão de Fogo Lendário. O majestoso Pokémon de tipo Fogo/Voador, é uma das criaturas mais icônicas da franquia Pokémon. Evolução final de Charmander, Charizard é conhecido por seu poder de fogo avassalador e suas majestosas asas que lhe permitem dominar os céus.',
+        link : 'https://www.pokemon.com/br/pokedex/charizard',
+        tags : 'fogo dragao dragão'
+    },
+
+    {
+        titulo : 'Venussaur',
+        descricao : 'Venusaur, O Gigante Planta. A imponente evolução final de Bulbasaur, é um Pokémon do tipo Planta/Veneno. Adornado por uma flor gigante em suas costas, Venusaur é um símbolo de força e resistência. Sua natureza calma e serena contrasta com o poder devastador de seus ataques de planta.',
+        link : 'https://www.pokemon.com/br/pokedex/venusaur',
+        tags : 'flor verde'
+    },
+
+    {
+        titulo : 'Blastoise',
+        descricao : 'Blastoise, O Canhão de Água Definitivo, o imponente Pokémon do tipo Água, é a evolução final de Squirtle. Conhecido por sua poderosa concha, que funciona como um canhão de água de alta pressão, Blastoise é uma força a ser reconhecida em qualquer batalha. Sua aparência robusta e sua natureza estratégica fazem dele um dos Pokémon mais respeitados nos ginásios tipo Água.',
+        link : 'https://www.pokemon.com/br/pokedex/blastoise',
+        tags : "agua tartaruga"
+    },
+    
+];
+
+
+
